593d6b962b34e907a3983fb9c204139244b6c4eb
Javier <ing.jmpinillag@gmail.com>
2021-01-26 13:58:07 +0100

    Registro de los modelos al sitio del admin

--- video_club/api/admin.py
+++ video_club/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Cinta)
+admin.site.register(Cliente)
+admin.site.register(Sucursal)
+admin.site.register(Prestamo)
