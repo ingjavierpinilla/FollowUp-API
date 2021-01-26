1516782817229d5a0e13c5fa7950f7e67b538495
Javier <ing.jmpinillag@gmail.com>
2021-01-26 11:47:47 +0100

    Cambio de la clave privada

--- video_club/video_club/settings.py
+++ video_club/video_club/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from keys import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '14(1if(i0=f6_kxr817o(we$#y3i@q8iw+@3k*c!1*y#f*=d)q'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
