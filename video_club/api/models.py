f0e5c0df50439584384d99b84f64a4ff2cabe920
Javier <ing.jmpinillag@gmail.com>
2021-01-26 19:16:22 +0100

    Cambio de modelos Llaves foraneas agregadas

--- video_club/api/models.py
+++ video_club/api/models.py
@@ -6,9 +6,9 @@ class Cinta(models.Model):
     disponible = models.BooleanField(default=True)
     def __str__(self):
         if self.disponible:
-            return f'{self.titulo}, disponible'
+            return f'{self.id} {self.titulo}, disponible'
         else:
-            return f'{self.titulo}, no disponible'
+            return f'{self.id} {self.titulo}, no disponible'
 
 class Cliente(models.Model):
     nombres = models.CharField(max_length=60)
@@ -22,10 +22,16 @@ class Sucursal(models.Model):
     nombre = models.CharField(max_length=60)
     ciudad = models.CharField(max_length=60)
     direccion = models.CharField(max_length=60, null=True, blank=True)
+    def __str__(self):
+        return f'{self.id} {self.nombre}'
 
 class Prestamo(models.Model):
-    codigo_cliente = models.IntegerField(null = False)
-    codigo_cinta = models.IntegerField(null = False)
-    codigo_sucursal = models.IntegerField(null = False)
+    codigo_cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
+    codigo_cinta = models.ForeignKey(Cinta,on_delete=models.CASCADE)
+    codigo_sucursal = models.ForeignKey(Sucursal,on_delete=models.CASCADE)
     fecha_prestamo = models.DateTimeField(auto_now_add=True)
     fecha_entrega = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f'Prestamo cinta: {self.codigo_cinta}, S {self.codigo_sucursal}, F {self.fecha_prestamo}'
