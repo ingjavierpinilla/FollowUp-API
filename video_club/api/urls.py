9a4def8bbf89133f11dddb6cbf635e59d3afac30
Javier <ing.jmpinillag@gmail.com>
2021-01-26 19:16:37 +0100

    url para cintas disponibles

--- video_club/api/urls.py
+++ video_club/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
-from .views import ClienteView
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import ClienteList, SucursalList
 
 urlpatterns = [
-    path('cliente', ClienteView.as_view())
+    path('disponibles/', disponibles.as_view()),
+    path('cliente/', ClienteList.as_view()),
+    path('sucursal/', SucursalList.as_view()),
+
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
