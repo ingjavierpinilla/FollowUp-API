42123273c8809c56bbe3ecbae01b280057caaf64
Javier <ing.jmpinillag@gmail.com>
2021-01-27 10:32:17 +0100

    Serializers sin suar borrados

--- video_club/api/serializer.py
+++ video_club/api/serializer.py
@@ -1,28 +1,13 @@
 from rest_framework import serializers
-from .models import Cinta, Cliente, Sucursal, Prestamo
+from .models import Cinta, Sucursal
 
 class CintaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cinta
         fields = ('id', 'titulo', 'valor', 'disponible')
 
-class ClienteSerializer (serializers.ModelSerializer):
-    class Meta:
-        model = Cliente
-        fields = ('id', 'nombres', 'apellidos', 'direccion')
-
 class SucursalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sucursal
         fields = ('id','nombre','ciudad','direccion')
 
-class PrestamoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Prestamo
-        fields =('id', 'codigo_cliente', 'codigo_cinta', 'codigo_sucursal',
-        'fecha_prestamo', 'fecha_entrega')
-
-class VentasDiariasSerializer(serializers.Serializer):
-    codigo_sucursal = serializers.Field(source='codigo_sucursal')
-    cintas_alquiladas = serializers.Field(source='total')
-    #clientId = serializers.Field()
