06ce7d5ab88cb1005a900330c09294f4d97fedbd
Javier <ing.jmpinillag@gmail.com>
2021-01-27 18:13:54 +0100

    El pretamo puede afectar la disponibilidad de la cinta

--- video_club/api/models.py
+++ video_club/api/models.py
@@ -4,6 +4,7 @@ class Cinta(models.Model):
     titulo = models.CharField(max_length = 60, null = False)
     valor = models.FloatField(default = 0.0)
     disponible = models.BooleanField(default=True)
+    
     def __str__(self):
         if self.disponible:
             return f'{self.id} {self.titulo}, disponible'
@@ -22,6 +23,7 @@ class Sucursal(models.Model):
     nombre = models.CharField(max_length=60)
     ciudad = models.CharField(max_length=60)
     direccion = models.CharField(max_length=60, null=True, blank=True)
+    
     def __str__(self):
         return f'{self.id} {self.nombre}'
 
@@ -32,6 +34,18 @@ class Prestamo(models.Model):
     fecha_prestamo = models.DateTimeField()
     fecha_entrega = models.DateTimeField(null=True, blank=True)
 
-
     def __str__(self):
-        return f'Prestamo {self.codigo_cinta}'
+        return f'Prestamo {self.codigo_sucursal.pk}'
+
+    def changeCinta(self, disp):
+        cinta = Cinta.objects.filter(id=self.codigo_cinta.pk)
+        for i in range(len(cinta)):
+            cinta[i].disponible = disp
+            cinta[i].save()
+    
+    def save(self):
+        if self.fecha_entrega:
+            self.changeCinta(True)
+        else:
+            self.changeCinta(False)
+        super(Prestamo, self).save()
