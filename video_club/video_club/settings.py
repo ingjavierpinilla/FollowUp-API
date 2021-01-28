3e391a9bed7a642deddb9f9b8fc96d9b0ef29637
Javier <ing.jmpinillag@gmail.com>
2021-01-28 13:35:35 +0100

    cambio a base local

--- video_club/video_club/settings.py
+++ video_club/video_club/settings.py
@@ -79,8 +79,7 @@ WSGI_APPLICATION = 'video_club.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
+"""
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ingjmpinillag$video_club_db',
@@ -89,6 +88,14 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
+"""
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 }
 
 # Password validation
