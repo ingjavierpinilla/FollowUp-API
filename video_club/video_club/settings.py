a03c2d4147a4edaec247ddbbac8f7ca27fbae15d
Javier <ing.jmpinillag@gmail.com>
2021-01-28 15:13:20 +0100

    conexion a mysqlserver

--- video_club/video_club/settings.py
+++ video_club/video_club/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'video_club.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'video_club_db',
+        'USER': 'root',
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 
 }
